e58ee264e7b9da7115a92ccb5bdb87bfcdf206ac
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-09-07 10:33:49 -0500

    Testing

---
+++ src/test/java/com/openbootcamp/demorest/DemoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbootcamp.demorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
