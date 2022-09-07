1a591419a7bcf37ef78fd3aedb70d5eb5a90b9d8
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-09-07 10:33:36 -0500

    Main file

---
+++ src/main/java/com/openbootcamp/demorest/DemoRestApplication.java
@@ -0,0 +1,13 @@
+package com.openbootcamp.demorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(DemoRestApplication.class, args);
+  }
+  
+}
